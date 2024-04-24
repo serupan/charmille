e909b5b905de680bc0d771b6958f76c48a44894e
serupan <xserupanx@outlook.com>
2024-04-25 00:55:30 +0200

    modif astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,9 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  // site: "https://example.com",
   integrations: [db()],
-  output: "hybrid",
+  output: "server",
   adapter: netlify()
 });
 
--- db/seed.ts
+++ db/seed.ts
@@ -3,7 +3,7 @@ import { Like, db } from 'astro:db';
 // https://astro.build/db/seed
 export default async function seed() {
 	await db.insert(Like).values([
-		{ id: 2, title: "le-probleme-du-bac-a-sable" },
-		{ id: 3, title: "classe-le-vagabond-exploirateurs-de-bruines" },
+		{ id: 1, title: "le-probleme-du-bac-a-sable" },
+		{ id: 2, title: "classe-le-vagabond-exploirateurs-de-bruines" },
 	]);
 }
