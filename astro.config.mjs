c39637c7043fa2ad7bdcc07d1d7c144e93fc4106
serupan <xserupanx@outlook.com>
2024-04-24 18:25:36 +0200

    ajout typescript et astro js check

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import db from "@astrojs/db";
-import node from "@astrojs/node";
+import netlify from "@astrojs/netlify";
+
 
 // https://astro.build/config
 export default defineConfig({
-  site:"https://example.com",
+  site: "https://example.com",
   integrations: [db()],
   output: "hybrid",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: netlify()
 });
