0954b85d4eff3eb5cba0d841eeb94b42080975a0
serupan <xserupanx@outlook.com>
2024-04-26 12:01:30 +0200

    config rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,24 +1,12 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import sanitizeHtml from 'sanitize-html';
-import MarkdownIt from 'markdown-it';
-const parser = new MarkdownIt();
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
-  const posts = await getCollection('posts');
   return rss({
-    title: 'Dragon Odile',
-    description: 'Blog de Serupan',
+    title: 'Blog de Buzz',
+    description: 'Le guide des étoiles',
     site: context.site,
-    items: posts.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.pubDate,
-      content: sanitizeHtml(parser.render(post.body), {
-        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
-      }),
-
-      link: `/posts/${post.slug}/`,
-    })),
-    customData: `<language>fr-FR</language>`,
+    items: await pagesGlobToRssItems(
+      import.meta.glob('./posts/*.{md,mdx}'),
+    ),
   });
 }
