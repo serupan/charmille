8b3aa4f655d92cac00121a3c61d24f6ee597081f
serupan <xserupanx@outlook.com>
2024-04-26 11:45:31 +0200

    config flux rss et ajout packages

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,24 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function GET(context) {
+  const posts = await getCollection('posts');
   return rss({
     title: 'Dragon Odile',
     description: 'Blog de Serupan',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-    customData: `<language>en-us</language>`,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+      }),
+
+      link: `/posts/${post.slug}/`,
+    })),
+    customData: `<language>fr-FR</language>`,
   });
 }
