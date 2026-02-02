95dce1496a205f98046dc50d0314673de39e4cc4
serupan <xserupanx@outlook.com>
2026-02-02 15:49:49 +0100

    retrait bouton like

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify";
-import db from "@astrojs/db";
 
 export default defineConfig({
   site: "https://pharefantome.com/",
   output: "server",
   adapter: netlify(),
-  integrations: [db()],
 });
